0dcac1596586122bf5d5d2f012e7248bdb3f2aa5
Can Yakar <canyakar9797@gmail.com>
2022-11-06 20:07:10 +0300

    feed design completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,18 @@ import { useState, useEffect } from "react"
 import { Box, Stack, Typography } from "@mui/material"
 import {Sidebar, Videos} from "./"
 
+import { fetchFromAPI } from "../utils/fetchFromAPI"
+
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("New")
+  const [videos, setVideos] = useState([])
+
+  useEffect( () => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then( (data) => setVideos(data.items))
+  }, [selectedCategory] )
+
   return (
     <Stack sx={{flexDirection: { sx: "column", md: "row" }}}>
       {/* Sidebar */}
@@ -13,7 +24,7 @@ const Feed = () => {
           px: {sx: 0, md: 2}
         }}
       >
-        <Sidebar />
+        <Sidebar selectedCategory = {selectedCategory} setSelectedCategory= {setSelectedCategory} />
 
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
           Copyright 2022 Wdaw Media
@@ -21,11 +32,11 @@ const Feed = () => {
       </Box>
     {/* Videos */}
       <Box p={2} sx={{ overflowY: 'auto', height: "90vh", flex: 2}} >
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: "white"}}>
-          New
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: "white", borderBottom: '1px solid #3d3d3d'}}>
+          {selectedCategory}
           <span style={{color: '#f31503'}} > Videos</span>
         </Typography>
-        <Videos videos= {[]} />
+        <Videos videos= {videos} />
       </Box>
     </Stack>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,7 @@
 import { Stack } from "@mui/material"
 import {categories} from '../utils/constants';
 
-const Sidebar = () => {
-
-    let selectedCategory = "New";
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
 
   return (
     <Stack
@@ -15,7 +13,7 @@ const Sidebar = () => {
         }}
     >
         {categories.map((category)=>(
-            <button className="category-btn" key={category.name}
+            <button className="category-btn" key={category.name} onClick={() => setSelectedCategory(category.name)}
             style={{background: category.name === selectedCategory && '#fc1503', color: "white" }} > 
                 <span style= {{color: category.name === selectedCategory ? "white" : "red", marginRight: "10px"}} > {category.icon} </span>
                 <span style={{opacity: category.name === selectedCategory ? 1 : 0.8}} > {category.name} </span>
